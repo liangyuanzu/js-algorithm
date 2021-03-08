9e818077d848a5d56529afab0cbf6d90179752ac
liangyuanzu <liangyuanzu@gmail.com>
2021-03-08 19:12:57 +0800

    feat: 用对象模拟队列

--- queue/index.js
+++ queue/index.js
@@ -27,17 +27,11 @@ class ArrQueue {
   clear() {
     this.items = [];
   }
+  // 转字符串
+  toString() {
+    return this.items.toString();
+  }
 }
-
-// let q = new Queue();
-// q.enqueue(3);
-// q.enqueue(2);
-// q.enqueue(4);
-// q.dequeue();
-// let t = q.peek();
-// let isEmpty = q.isEmpty();
-// let size = q.size();
-// q.clear();
 
 // 用对象模拟队列
 class ObjQueue {
@@ -51,5 +45,40 @@ class ObjQueue {
     this.items[this.count] = element;
     this.count += 1;
   }
+  // 检查队列是否为空
+  isEmpty() {
+    return this.count - this.lowestCount === 0;
+  }
+  // 返回队列长度
+  size() {
+    return this.count - this.lowestCount;
+  }
   // 出队
+  dequeue() {
+    if (this.isEmpty()) return undefined;
+    const res = this.items[this.lowestCount];
+    delete this.items[this.lowestCount];
+    this.lowestCount += 1;
+    return res;
+  }
+  // 查看队头元素
+  peek() {
+    if (this.isEmpty()) return undefined;
+    return this.items[this.lowestCount];
+  }
+  // 清空队列
+  clear() {
+    this.count = 0;
+    this.lowestCount = 0;
+    this.items = {};
+  }
+  // 转字符串
+  toString() {
+    if (this.isEmpty()) return "";
+    let str = `${this.items[this.lowestCount]}`;
+    for (let i = this.lowestCount + 1; i < this.count; i += 1) {
+      str = `${str}, ${this.items[i]}`;
+    }
+    return str;
+  }
 }
