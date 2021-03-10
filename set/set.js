2b370f2c57cff151a932a3c307765e4b06e56038
liangyuanzu <liangyuanzu@gmail.com>
2021-03-10 23:49:51 +0800

    feat: set 相关操作

---
+++ set/set.js
@@ -0,0 +1,48 @@
+// set 相关操作
+// add
+let set = new Set();
+set.add(1);
+set.add(5);
+set.add(5);
+set.add("text");
+let obj = { a: 1, b: 2 };
+set.add(obj);
+set.add({ a: 1, b: 2 });
+
+// has
+const has = set.has(obj);
+
+// delete
+set.delete(5);
+
+// 迭代
+for (let val of set) {
+  console.log(val);
+}
+
+set.forEach((val) => console.log(val));
+
+for (let key of set.keys()) {
+  console.log(key);
+}
+
+for (let val of set.values()) {
+  console.log(val);
+}
+
+for (let [key, val] of set.entries()) {
+  console.log(key, val);
+}
+
+// Set -> Array
+let arr = [...set];
+arr = Array.from(set);
+
+// Array -> Set
+const set2 = new Set([1, 2, 3]);
+
+// 交集
+const intersection = new Set([...set].filter((i) => set2.has(i)));
+
+// 差集
+const difference = new Set([...set].filter((i) => !set2.has(i)));
