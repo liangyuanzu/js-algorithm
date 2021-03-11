eec6dd092cb0a6134132050618a71cccbc45b43d
liangyuanzu <liangyuanzu@gmail.com>
2021-03-11 08:39:36 +0800

    feat: map 相关操作

---
+++ map/index.js
@@ -0,0 +1,18 @@
+// map 相关操作
+const m = new Map();
+
+// 增
+m.set("a", "aa");
+m.set("b", "bb");
+
+// 删
+m.delete("b");
+
+// 清空
+m.clear();
+
+// 改
+m.set("a", "aaa");
+
+// 查
+const a = m.get("a");
